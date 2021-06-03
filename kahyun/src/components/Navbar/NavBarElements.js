0f715e583d0f3523605e5eaebd2344151cbf67dc
kahyunnam <69875166+kahyunnam@users.noreply.github.com>
2021-06-03 16:54:35 -0400

    interactive navbar finished

--- kahyun/src/components/Navbar/NavBarElements.js
+++ kahyun/src/components/Navbar/NavBarElements.js
@@ -96,7 +96,7 @@ export const NavBtn = styled.nav`
 `;
 
 export const NavBtnLink = styled(LinkR)`
-border-radius: 50px;
+  border-radius: 50px;
   background: #01bf71;
   white-space: nowrap;
   padding: 10px 22px;
--- kahyun/src/components/Sidebar/index.js
+++ kahyun/src/components/Sidebar/index.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink, SideBtnWrap, SidebarRoute } from './SidebarElement';
 
-const index = () => {
+const Sidebar = ({ isOpen, toggle }) => {
   return (
-    <div>
-
-    </div>
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle} >
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about">
+            About
+          </SidebarLink>
+          <SidebarLink to="discover">
+            Discover
+          </SidebarLink>
+          <SidebarLink to="services">
+            Services
+          </SidebarLink>
+          <SidebarLink to="signup">
+            Signup
+          </SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to="/signin">Sign In</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
   )
 }
 
-export default index
+export default Sidebar;
